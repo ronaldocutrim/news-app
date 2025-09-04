3036092195c36961f1486a477053ad3da0e2f7c7
Ronaldo <ronaldocutrim@gmail.com>
2025-09-03 22:52:59 -0300

    resolve index

--- src/core/feed/index.ts
+++ src/core/feed/index.ts
@@ -1,10 +1,1 @@
-// Feed Core Module
-export * from './model/NewsArticle';
-export * from './model/NewsService';
-export * from './viewmodel/FeedViewModel';
-
-// Views
-export { NewsCard } from './view/NewsCard';
-export { NewsCardView } from './view/NewsCardView';
 export { FeedListScreen } from './view/FeedListScreen';
-export { FeedListView } from './view/FeedListView';
